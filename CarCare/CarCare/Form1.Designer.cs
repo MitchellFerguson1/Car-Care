6dd92a1f5a0633b9309930ead4f32cb9666b476c
Mitchell <Mitchell.Ferguson1998@gmail.com>
2016-12-01 21:37:49 -0500

    Testing stuff

--- CarCare/CarCare/Form1.Designer.cs
+++ CarCare/CarCare/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.testLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testLbl
+            // 
+            this.testLbl.AutoSize = true;
+            this.testLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testLbl.Location = new System.Drawing.Point(523, 155);
+            this.testLbl.Name = "testLbl";
+            this.testLbl.Size = new System.Drawing.Size(262, 97);
+            this.testLbl.TabIndex = 0;
+            this.testLbl.Text = "TEST";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 485);
+            this.Controls.Add(this.testLbl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label testLbl;
     }
 }
 
